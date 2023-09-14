4fbbb9862554c06ca8ffadcf459e4cbe00751d54
Charlielv04 <carloslv04@gmail.com>
2023-09-14 11:25:29 +0200

    Add drive file to init

--- bx_utils/__init__.py
+++ bx_utils/__init__.py
@@ -2,5 +2,6 @@ from . import config
 from . import db
 from . import gc
 from . import telegram
+from . import drive
 from .log import logger
 from . import passwords
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(requirements)
 
 setup(
     name="bx-utils",
-    version='0.13',
+    version='0.14',
     author="BX-bot-ecosystem",
     author_email="something@something.something",
     packages=find_packages(),
