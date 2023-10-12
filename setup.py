dce2b31a979254567c0cbf004aea4beabd14705e
Charlielv04 <Carloslv04@gmail.com>
2023-10-12 20:26:04 +0200

    Bug fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ print(requirements)
 
 setup(
     name="bx-utils",
-    version='0.23',
+    version='0.231',
     author="BX-bot-ecosystem",
     author_email="something@something.something",
     packages=find_packages(),
